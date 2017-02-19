2132647f383a2ea94ab5ea94bcf9ed97e307964e
Brent Woodcock <brentwoodcock@gmail.com>
2017-02-19 14:24:39 -0600

    modified contact configuration details

--- contact.php
+++ contact.php
@@ -3,8 +3,8 @@
 /**
  * configure here
  */
-$from = 'IT WORKER <brentwoodcock@gmail.com>';
-$sendTo = 'IT WORKER <brentwoodcock@gmail.com>';
+$from = '<admin@brentwoodcock.com>';
+$sendTo = '<brentwoodcock@gmail.com>';
 $subject = 'New message from contact form';
 $fields = array('name' => 'Name', 'surname' => 'Surname', 'phone' => 'Phone', 'email' => 'Email', 'message' => 'Message');
 $htmlHeader = '';
