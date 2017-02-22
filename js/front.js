01b1da1c106393dbb3ac46d87a1848712e684579
Brent Woodcock <brentwoodcock@gmail.com>
2017-02-21 19:21:08 -0600

    added resume

--- js/front.js
+++ js/front.js
@@ -82,13 +82,15 @@ function animations() {
 
 function sliding() {
     $('.scrollTo, #navigation a').click(function (event) {
-	event.preventDefault();
-	var full_url = this.href;
-	var parts = full_url.split("#");
-	var trgt = parts[1];
+    	if (event.target.id != 'resume'){
+    		event.preventDefault();
+			var full_url = this.href;
+			var parts = full_url.split("#");
+			var trgt = parts[1];
 
-	$('body').scrollTo($('#' + trgt), 800, {offset: -80});
-
+			$('body').scrollTo($('#' + trgt), 800, {offset: -80});
+    	}
+	
     });
 }
 
